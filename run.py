d583d4d408d0a3dc2db2b745ef5b6891a043ea65
Bruno Conde Kind <condekind@users.noreply.github.com>
2024-09-09 11:39:12 -0300

    Initial commit

---
+++ run.py
@@ -0,0 +1,40 @@
+import polars as pl
+from io_plugin import new_bernoulli, new_uniform, new_fibonacci, scan_random, scan_sequence
+
+
+lf = scan_random(
+    [
+        new_bernoulli("b0.5", p=0.5, seed=1),
+        new_bernoulli("b0.1", p=0.1, seed=2),
+        new_uniform("uniform", low=10, high=100, dtype=pl.Int32, seed=2),
+    ]
+)
+
+#print(lf.collect())
+
+# predicate pushdown
+assert lf.filter(pl.col("b0.5")).collect()["b0.5"].all()
+
+# slice pushdown
+assert lf.head(100).collect().shape == (100, 3)
+
+# projection pushdown
+out = lf.select("uniform", "b0.1").collect()
+assert out.shape == (1000, 2)
+assert out.columns == ["uniform", "b0.1"]
+
+lf = scan_sequence(
+    [
+        new_fibonacci("fib_pos", first=1, second=1),
+        new_fibonacci("fib_neg", first=-1, second=-1),
+    ]
+)
+
+#lf = lf.filter(pl.col("fib_pos") > 10)
+
+print()
+print("lf.head(10).collect():")
+print(lf.head(10).collect())
+print()
+print("lf.head(10).collect():")
+print(lf.head(10).collect())
---
+++ src/fibonacci.rs
@@ -0,0 +1,73 @@
+use polars::prelude::*;
+use pyo3::{pyclass, pyfunction};
+use pyo3_polars::export::polars_core::datatypes::DataType;
+use pyo3_polars::export::polars_core::prelude::Series;
+use std::sync::Mutex;
+
+#[pyclass]
+#[derive(Clone)]
+pub struct PySeq(
+    pub Arc<Mutex<Box<dyn SeqProducer>>>,
+);
+
+pub trait SeqProducer: Send {
+    fn name(&self) -> &str;
+
+    fn dtype(&self) -> DataType;
+
+    fn next_n(&mut self, n: usize) -> Series;
+}
+
+struct FibonacciProducer {
+    name: String,
+    last_two: [i64; 2],
+}
+
+fn new_fib_impl(name: String, first: i64, second: i64) -> FibonacciProducer {
+    FibonacciProducer {
+        name,
+        last_two: [first, second],
+    }
+}
+
+impl SeqProducer for FibonacciProducer {
+    fn name(&self) -> &str {
+        &self.name
+    }
+
+    fn dtype(&self) -> DataType {
+        return DataType::Int64;
+    }
+
+    fn next_n(&mut self, n: usize) -> Series {
+        // Output/result vector
+        let mut out = Vec::with_capacity(n);
+
+        // Push the last two numbers stored to the sequence. Without this, the
+        // first two numbers supplied (e.g., [1, 1]) wouldn't show in the result
+        out.push(self.last_two[0]);
+        out.push(self.last_two[1]);
+
+        // Calculate and push the next n-2, since we already pushed 2 above
+        for _ in 0..n-2 {
+            let next = self.last_two[0].checked_add(self.last_two[1]).unwrap_or(self.last_two[1]);
+            out.push(next);
+            self.last_two = [self.last_two[1], next];
+        }
+
+        // Calculate next two numbers that occupy last_two, for future purposes
+        for _ in 0..2 {
+            let next = self.last_two[0].checked_add(self.last_two[1]).unwrap_or(self.last_two[1]);
+            self.last_two = [self.last_two[1], next];
+        }
+
+        Series::from_vec(self.name(), out)
+    }
+}
+
+#[pyfunction]
+pub fn new_fibonacci(name: String, first: i64, second: i64) -> PySeq {
+    let res = Box::new(new_fib_impl(name, first, second)) as Box<dyn SeqProducer>;
+    PySeq(Arc::new(Mutex::new(res)))
+}
+
